8f74e3b697e9ea1fd8fb5b2efc189f0f93e53411
Braden Sten <b.sten88@gmail.com>
2017-08-03 15:52:58 -0600

    implemented observable and connected custom rails api

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,35 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
 	moduleId: module.id,
 	selector: 'documents',
 	templateUrl: 'documents.component.html',
-    styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
 	pageTitle: string = "Document Dashboard"
-	documents: Document[] = [
-    {
-    	title: "My First Doc",
-    	description: 'fdsafdfad',
-    	file_url: 'http://google.com',
-    	updated_at: '07/31/17',
-    	image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
-    },
-    {
-    	title: "My Second Doc",
-    	description: 'fdsafdfad',
-    	file_url: 'http://google.com',
-    	updated_at: '07/31/17',
-    	image_url: 'http://www.java-studios.com/wp-content/uploads/2016/07/freelancing.jpg'
-    },
-    {
-    	title: "My Last Doc",
-    	description: 'fdsafdfad',
-    	file_url: 'http://google.com',
-    	updated_at: '07/31/17',
-    	image_url: 'https://membermouse.com/wp-content/uploads/2016/09/Freelancer-Liability-Featured.png'
-    }
-	]
+	documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService;
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000)
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
